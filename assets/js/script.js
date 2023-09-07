f31a23f457b0a8000be36356ae709192e5d24acb
xtian <benedictramirez78@gmail.com>
2023-09-08 00:21:39 +0800

    added projects

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+let calcScrollValue = () => {
+  let scrollProgress = document.getElementById("progress");
+  let progressValue = document.getElementById("progress-value");
+  let pos = document.documentElement.scrollTop;
+  let calcHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrollValue = Math.round((pos * 100) / calcHeight);
+  if (pos > 200) {
+    scrollProgress.style.display = "grid";
+  } else {
+    scrollProgress.style.display = "none";
+  }
+  scrollProgress.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+  scrollProgress.style.background = `conic-gradient(#6b7280 ${scrollValue}%, #212121 ${scrollValue}%)`;
+};    
+  
+  window.onscroll = calcScrollValue;
+  window.onload = calcScrollValue;
